88fb5af199be68e403d60584601160064acc7d1f
Ramos04 <vince.ramos4@gmail.com>
2021-10-22 16:24:29 -0700

    Removed the testing folder

--- Testing/os_info.ps1
+++
@@ -1,9 +0,0 @@
-#Requires -RunAsAdministrator
-
-# https://stackoverflow.com/questions/48592120/how-do-i-find-out-from-powershell-if-i-am-on-a-server-or-workstation
-
-# ProductType
-#   Work Station (1)
-#   Domain Controller (2)
-#   Server (3)
-$productType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
